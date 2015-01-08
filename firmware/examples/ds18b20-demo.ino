6e4f1a4e2ef7d9a5f163dd4603c7cfa70878ed82
waspinator <patrickwasp@gmail.com>
2015-01-07 22:54:22 -0500

    first

---
+++ firmware/examples/ds18b20-demo.ino
@@ -0,0 +1,125 @@
+// This #include statement was automatically added by the Spark IDE.
+#include "OneWire.h"
+
+// The DS18B20 digital thermometer provides 9 to 12 bit Celsius temperature measurements
+// 1-Wire Interface 
+// 3.3 - 5V 
+// -10°C to +85°C @ ±0.5°C resolution 
+// http://datasheets.maximintegrated.com/en/ds/DS18B20.pdf
+// PIN 1 -> GND
+// PIN 2 -> Data with a pull up resistor. All sensors share this pin.
+// PIN 3 -> Vdd
+
+ 
+float celsius;
+char cel[8];
+OneWire ds = OneWire(D3);  // on pin 10 (a 4.7K resistor is necessary)
+ 
+void setup() {
+  Serial.begin(9600);
+  //Spark.variable("temp",&cel,STRING);
+}
+ 
+void loop() {
+  byte i;
+  byte present = 0;
+  byte type_s;
+  byte data[12];
+  byte addr[8];
+  float celsius, fahrenheit;
+ 
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(5000);
+    ds.reset();
+    ds.skip();
+    ds.write(0x44);
+    Serial.println("Converting temp on all probes");
+    Serial.println();
+    delay(1000);
+    return;
+  }
+ 
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+ 
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return;
+  }
+ 
+         // start conversion, with parasite power on at the end
+ 
+ 
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE);         // Read Scratchpad
+ 
+  Serial.print("  Data = ");
+  Serial.print(present, HEX);
+  Serial.print(" ");
+  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+    data[i] = ds.read();
+    Serial.print(data[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.print(" CRC=");
+  Serial.print(OneWire::crc8(data, 8), HEX);
+  Serial.println();
+ 
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  if (type_s) {
+    raw = raw << 3; // 9 bit resolution default
+    if (data[7] == 0x10) {
+      // "count remain" gives full 12 bit resolution
+      raw = (raw & 0xFFF0) + 12 - data[6];
+    }
+  } else {
+    byte cfg = (data[4] & 0x60);
+    // at lower res, the low bits are undefined, so let's zero them
+    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    //// default is 12 bit resolution, 750 ms conversion time
+  }
+  celsius = (float)raw / 16.0;
+  sprintf(cel,"%f",celsius);
+  fahrenheit = celsius * 1.8 + 32.0;
+  Serial.print("  Temperature = ");
+  Serial.print(celsius);
+  Serial.print(" Celsius, ");
+  Serial.print(fahrenheit);
+  Serial.println(" Fahrenheit");
+ 
+ 
+}
+
